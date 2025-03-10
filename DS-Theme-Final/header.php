cf4c7e1d4c7e5ae25809949e5e408000a6931d17
Ylliii1 <yllbytyqi180@gmail.com>
2025-03-10 17:44:10 +0100

    commit

---
+++ DS-Theme-Final/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DS-Theme-Final</title>
+</head>
+<body <?php body_class(); ?>>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam!</p>
+    <div id="page" class="site">
+        <header>
--- DS-Theme-Final/index.php
+++ DS-Theme-Final/index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DS-Theme-Final</title>
-</head>
-<body <?php body_class(); ?>>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quam!</p>
-    <div id="page" class="site">
-        <header>
+<?php get_header(); ?>
             <section class="top-bar">
                 <div class="logo">
                     Logo
@@ -40,10 +30,6 @@
             </div>
         </div>
         <!-- Body -->
-        <footer class="site-footer">
-            Footer
-        </footer>
-    </div>
-</body>
-</html>
-        <!-- Footer -->
+        
+
+        <?php get_footer(); ?>
