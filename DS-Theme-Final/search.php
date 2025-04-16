d7e97206577c3a0182d16fa857e6e86b1e5b4e49
Ylliii1 <yllbytyqi180@gmail.com>
2025-04-16 17:47:39 +0200

    .
    .

--- DS-Theme-Final/search.php
+++ DS-Theme-Final/search.php
@@ -1,23 +1,26 @@
-<?php get_header() ?>
+<?php get_header() ?><?php get_header() ?>
 
 
 <div id="primary">
     <div id="main">
         <div class="container">
+            <h3> Search results for: <?php echo get_search_query(); ?> </h3>
             <?php
+            get_search_form();
+
             while( have_posts() ):
                 the_post();
             ?>
             <article id="post-<?php the_ID();?>" <?php post_class(); ?>>
                 <header>
-                    <h1><?php the_title(); ?></h1>
+                    <h2><?php the_title(); ?></h2>
+                    <?php if('post'== get_post_type() ): ?>
                     <div class="meta-info">
                         <p>Posted in: <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
                         <p>Categories: <?php the_category( ' ' ); ?></p>
                         <p>Tags: <?php the_tags('', ', ') ?></p>
-
-
                     </div>
+                    <?php endif; ?>
                 </header>
                 <div class="content">
                     <?php the_content(); ?>
